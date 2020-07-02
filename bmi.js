ab0b906b9f9b32f60416d93b78195f08674f8628
beardedbinary <carlball28@gmail.com>
2020-07-02 12:46:59 +0100

    main styling completed

--- bmi.js
+++ bmi.js
@@ -9,28 +9,30 @@ function bmicalc() {
   var bmi = (totalPounds / totalInchesSquared) * 703;
   var roundbmi = Math.round(bmi * 10) / 10;
   var result = document.getElementById("yourbmi");
+  var category = document.getElementById("category");
 
   if (roundbmi < 18.5) {
-    result.textContent =
-      "Your BMI is " + roundbmi + " You are classed as underweight";
+    result.textContent = "Your BMI is " + roundbmi;
+    category.textContent = "You are classed as underweight";
   } else if (roundbmi < 24.9) {
-    result.textContent =
-      "Your BMI is " + roundbmi + " You are classed as normal healthy weight";
+    result.textContent = "Your BMI is " + roundbmi;
+    category.textContent = "You are classed as normal healthy weight";
   } else if (roundbmi < 29.9) {
-    result.textContent =
-      "Your BMI is " + roundbmi + " You are classed as overweight";
+    result.textContent = "Your BMI is " + roundbmi;
+    category.textContent = "You are classed as overweight";
   } else if (roundbmi < 39.9) {
-    result.textContent =
-      "Your BMI is " + roundbmi + " You are classed as obese";
+    result.textContent = "Your BMI is " + roundbmi;
+    category.textContent = "You are classed as obese";
   } else if (roundbmi >= 40.0) {
-    result.textContent =
-      "Your BMI is " + roundbmi + " You are classed as morbidly obese";
+    result.textContent = "Your BMI is " + roundbmi;
+    category.textContent = "You are classed as morbidly obese";
   }
 }
 
 function resetPage() {
   document.getElementById("bmi-input").reset();
   document.getElementById("yourbmi").innerHTML = "";
+  document.getElementById("category").innerHTML = "";
 }
 
 // BMI = 703 ×
